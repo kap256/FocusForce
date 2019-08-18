362b0ddfc1291fecf1cdde7b693ae8910bdb8e86
カレプリ <kojikyo256@gmail.com>
2019-08-18 14:59:46 +0900

    GetForegroundWindowの差し替えを試みてはいる。

--- FFDllpp/FFDll.h
+++ FFDllpp/FFDll.h
@@ -5,3 +5,14 @@ EXPORT bool StartHook(void);
 EXPORT bool EndHook(void);
 
 extern HMODULE gModule;
+
+void OnAttach();
+void OnDetach();
+
+void Print(const TCHAR* message);
+
+void* RewriteFunction(const char* szRewriteModuleName, const char* szRewriteFunctionName, void* pRewriteFunctionPointer);
+void PrintFunctions();
+
+const int buf_size = 512;
+extern TCHAR buf[buf_size];
